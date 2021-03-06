f42e73f77ac47c589062bd73994ca49ee7583f45
faraamcode <abdulrasaqalagbede@gmail.com>
2021-03-06 22:00:32 +0100

    impotant notice added and styled

--- src/maincontent.jsx
+++ src/maincontent.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { FaBars } from 'react-icons/fa';
+import {useState} from 'react'
+import { FaBars, FaPhoneAlt, FaRegCalendarAlt} from 'react-icons/fa';
 import { FiSettings, FiSearch } from 'react-icons/fi';
 import { GrNotification } from 'react-icons/gr';
 import { useGlobalContext } from './context';
-const Content = () => {
+import {MdEmail, MdContactMail} from 'react-icons/md'
+import {days, months} from './date'
+const dat = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga veritatis delectus beatae voluptas maxime enim ipsum labore eveniet repudiandae natus ad, tempora quaerat animi ipsa molestiae porro non laudantium adipisci dolorem consequatur aperiam. Exercitationem adipisci, eveniet quibusdam dignissimos consequatur asperiores blanditiis architecto fugit sapiente, cumque velit. Maxime quae culpa nobis quasi ea aliquid! Excepturi deleniti ad similique commodi dolore placeat delectus quisquam odio quaerat, suscipit provident nihil laborum, incidunt doloribus aliquid magni temporibus! Consequuntur iusto assumenda illo vitae harum corrupti natus sint aliquam aut sunt non libero, earum quo, odio optio deserunt aspernatur possimus sed placeat? Repudiandae expedita asperiores similique."
 
-  
+const Content = () => {
+    const [info, setInfo] = useState(false)
+//     useEffect(()=>{
+//    setInfo(dat.slice(0, 100))
+//     }, [])
+    const format =(n)=>{
+    if(n==1 ||n == 21 ||n== 31){
+     return `${n}st`
+    }else if(n == 2 || n==22){
+      return `${n}nd`
+    }else if(n == 3 || n== 23){
+     return `${n}rd`
+    }
+    else{
+        return `${n}th`
+    }
+    
+}
+    const date = format(new Date().getDate())
+    const Year = new Date().getFullYear()
+    const month = months[new Date().getMonth()]
+    const day = days[new Date().getDay()]
+
+    console.log(date, Year, month, day);
   return (
     <div className="main-content">
        <div className="profile-container">
@@ -48,8 +74,50 @@ const Content = () => {
             </div>
            </div>
        </div>
-       <div className="other-details"></div>
-       <div className="footer"></div>
+       <div className="other-details">
+          <div className="contact-container">
+              <div className="contact-content">
+               <div className="contact contact-1">
+                   <MdEmail className="contact-logo"/>
+                   <h5>Email: </h5>
+                   <p>info@roemichsschools.com</p>
+               </div>
+               <div className="contact">
+                   <FaPhoneAlt className="contact-logo"/>
+                   <h5>Phone: </h5>
+                   <p>08035125100</p>
+               </div>
+              </div>
+              <div className="contacts-logo">
+               <MdContactMail/>
+              </div>
+          </div>
+          <div className="calender-container">
+          <div className="contact-content">
+               <div className="contact contact-3">
+                  
+                   <h3>{day}</h3>
+                   
+               </div>
+               <div className="contact">
+                 
+                   <h4>{`${date}, ${month} ${Year}`}</h4>
+               </div>
+              </div>
+              <div className="contacts-logo">
+               <FaRegCalendarAlt className="calendar-icon"/>
+              </div>
+          </div>
+       </div>
+       <div className="footer">
+           <h6>Important Notice</h6>
+           <p>{info ? dat : dat.slice(0, 200)}
+           <a href="" onClick={(e)=>{
+               e.preventDefault()
+               setInfo(!info)
+            }}>{!info ? "read more..." : "hide"}</a>
+            </p>
+       </div>
     </div>
   );
 };
