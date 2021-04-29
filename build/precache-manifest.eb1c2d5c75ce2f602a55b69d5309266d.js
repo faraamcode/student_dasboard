38fc75fb9c497e0b4672c31f7335c31dcb39d9f9
faraamcode <abdulrasaqalagbede@gmail.com>
2021-04-29 08:41:30 +0100

    testing

---
+++ build/precache-manifest.eb1c2d5c75ce2f602a55b69d5309266d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5bae613e0e4bcf4dda95971924db51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "117c584a94f3748a2342",
+    "url": "/static/css/main.2f5dc046.chunk.css"
+  },
+  {
+    "revision": "48719b9f6731f828218f",
+    "url": "/static/js/2.6e4b60cc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6e4b60cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117c584a94f3748a2342",
+    "url": "/static/js/main.5db397b0.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/static/media/logo.2bb7da65.svg"
+  }
+]);
